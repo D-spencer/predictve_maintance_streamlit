e73a0c254c30f7ff96edf799c7edf5eeff42af3f
Oluwadunsin <137607385+D-spencer@users.noreply.github.com>
2025-12-01 18:59:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import numpy as np
 st.title('🤖 Predictive Maintenance APP')
 
 st.info('This is a machine Learning App')
-df = pd.read_csv("Predictive_Maintance_Project/data/preprocessed_predictive_maintenance.csv")
+df = pd.read_csv("https://github.com/D-spencer/Predictive_Maintenance_ML_Project/main/Predictive_Maintance_Project/data/preprocessed_predictive_maintenance.csv")
 df 
 
 
