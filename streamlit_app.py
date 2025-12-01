34f25403d590909d9cb574bb9239239ab02cb0da
Oluwadunsin <137607385+D-spencer@users.noreply.github.com>
2025-12-01 19:15:13 +0100

    Use expanded section for raw data display
    Refactor data display to use expanded section.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,12 @@ import joblib
 import numpy as np
 
 st.title('🤖 Predictive Maintenance APP')
-
 st.info('This is a machine Learning App')
-df = pd.read_csv(
-  "https://raw.githubusercontent.com/D-spencer/Predictive_Maintenance_ML_Project/refs/heads/main/Predictive_Maintance_Project/data/preprocessed_predictive_maintenance.csv")
-df 
+
+with st.expanded('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv("https://raw.githubusercontent.com/D-spencer/Predictive_Maintenance_ML_Project/refs/heads/main/Predictive_Maintance_Project/data/preprocessed_predictive_maintenance.csv")
+  df 
 
 
 
