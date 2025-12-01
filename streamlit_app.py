954a7d41ec3fef12b2a9dc4051c318370f42c0db
Oluwadunsin <137607385+D-spencer@users.noreply.github.com>
2025-12-01 19:18:02 +0100

    Change 'st.expanded' to 'st.expander' in streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import numpy as np
 st.title('🤖 Predictive Maintenance APP')
 st.info('This is a machine Learning App')
 
-with st.expanded('Data'):
+with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv("https://raw.githubusercontent.com/D-spencer/Predictive_Maintenance_ML_Project/refs/heads/main/Predictive_Maintance_Project/data/preprocessed_predictive_maintenance.csv")
   df 
