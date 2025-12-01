27e0b69cdff2624d28413fe9a62dd346f188d5b1
Oluwadunsin <137607385+D-spencer@users.noreply.github.com>
2025-12-01 18:44:04 +0100

    Comment out Streamlit app components
    Comment out Streamlit imports and app title.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
-import streamlit as st
-import pandas as pd
+# import streamlit as st
+# import pandas as pd
 
-st.title('🤖 Predictive Maintenance APP')
+# st.title('🤖 Predictive Maintenance APP')
 
-st.info('This is a machine Learning App')
+# st.info('This is a machine Learning App')
 
-df= pd.read_csv('https://github.com/D-spencer/Predictive_Maintenance_ML_Project/blob/main/Predictive_Maintance_Project/data/predictive_maintenance.csv')
-df
+# df= pd.read_csv('https://github.com/D-spencer/Predictive_Maintenance_ML_Project/blob/main/Predictive_Maintance_Project/data/predictive_maintenance.csv')
+# df
