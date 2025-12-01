09888560871a17b129ee1293cffb5d8f511afe0d
Oluwadunsin <137607385+D-spencer@users.noreply.github.com>
2025-12-01 19:20:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.info('This is a machine Learning App')
 with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv("https://raw.githubusercontent.com/D-spencer/Predictive_Maintenance_ML_Project/refs/heads/main/Predictive_Maintance_Project/data/preprocessed_predictive_maintenance.csv")
-  df 
+  df
 
 
 
