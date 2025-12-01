b17e2ce667ed544f3481f63085f894be84a2ddf9
Oluwadunsin <137607385+D-spencer@users.noreply.github.com>
2025-12-01 18:51:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ st.title('🤖 Predictive Maintenance APP')
 
 st.info('This is a machine Learning App')
 
-df= pd.read_csv('https://github.com/D-spencer/Predictive_Maintenance_ML_Project/blob/main/Predictive_Maintance_Project/data/predictive_maintenance.csv')
-df
 
 
 
