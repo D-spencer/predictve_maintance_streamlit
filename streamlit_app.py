d1a927bbbb4bca4c4da883b9723bf0cb2b309198
Oluwadunsin <137607385+D-spencer@users.noreply.github.com>
2025-12-01 18:40:59 +0100

    Fix typo in read_csv method call

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title('🤖 Predictive Maintenance APP')
 
 st.info('This is a machine Learning App')
 
-df= pd.read.csv('https://github.com/D-spencer/Predictive_Maintenance_ML_Project/blob/main/Predictive_Maintance_Project/data/predictive_maintenance.csv')
+df= pd.read_csv('https://github.com/D-spencer/Predictive_Maintenance_ML_Project/blob/main/Predictive_Maintance_Project/data/predictive_maintenance.csv')
 df
