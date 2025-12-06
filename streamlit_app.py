248def124f408f61aa5d9fbf377fd6feb656a241
Oluwadunsin <137607385+D-spencer@users.noreply.github.com>
2025-12-06 08:49:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,15 +37,15 @@ with st.sidebar:
     
     submit = st.form_submit_button('Predict')
     
-   input_df  = pd.Dataframe([{
+    data = {
        'type': MachineType,
         'air_temperature': Air_Temperature,
         'process_temp': Process_Temperature,
         'rotational_speed_[rpm]': rotational_speed,
         'torque_[nm]': Torque,
         'tool_wear_[min]': tool_wear
-   }])
-    
+   }
+    input_df = pd.Dataframe(data)
     # input_pred = pd.concat([input_df, X_raw], axis=0)
 
 with st.expander('Input features'):
